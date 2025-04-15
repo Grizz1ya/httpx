3bb02e5b97625d22fd314396878205f446c650d3
Grizz1ya <ch1m3ra2000@gmail.com>
2025-04-15 23:48:15 +0300

    fix body encoding

--- response.go
+++ response.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/Anderson-Lu/gofasion/gofasion"
-	"golang.org/x/net/html/charset"
 )
 
 type Response struct {
@@ -17,23 +16,9 @@ func (r *Response) Json() *gofasion.Fasion {
 }
 
 func (r *Response) Text() string {
-	// defer r.response.Body.Close()
-
-	// content, err := io.ReadAll(r.response.Body)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// return string(content)
-
 	defer r.response.Body.Close()
 
-	utf8Reader, err := charset.NewReader(r.response.Body, r.response.Header.Get("Content-Type"))
-	if err != nil {
-		panic(err)
-	}
-
-	content, err := io.ReadAll(utf8Reader)
+	content, err := io.ReadAll(r.response.Body)
 	if err != nil {
 		panic(err)
 	}
